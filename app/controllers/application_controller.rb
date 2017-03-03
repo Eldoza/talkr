6a2fe82c9b0761f2787344e89fc36d7acad489ab
Chris Mendoza <cmend85@gmail.com>
2017-03-03 13:31:27 -0600

    Created posts and semi working controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_devise_parameters, if: :devise_controller?
 
-  def home
-      render html: "Hello world"
+  private
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up,   keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update,   keys: [:username, :email, :password, :password_confirmation])
   end
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @user = current_user if user_signed_in?
+    @posts = @user.post
   end
 end
